b8012ae35fa4fff2c1ce64d0beaf931b64de1e50
POO <miguel.carapaz@epn.edu.ec>
2023-01-17 11:46:04 -0500

    IMC

--- src/ventana1.java
+++ src/ventana1.java
@@ -1,6 +1,31 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ventana1 {
     private JButton okButton;
-    private JTextField ingreseSuNombreTextField;
+    private JTextField peso;
+    private JTextField altura;
+    private JPanel panel1;
+    private JLabel Respuesta;
+
+    public ventana1() {
+        okButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double numero1= Double.parseDouble(peso.getText());
+                double numero2= Double.parseDouble(altura.getText());
+                Respuesta.setText("Su IMC es:" + numero1/Math.pow(numero2,2));
+            }
+        });
+    }
+    public static void main(String[] args){
+        JFrame frame=new JFrame("ventana1");
+        frame.setContentPane(new ventana1().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
 }
+
